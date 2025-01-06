3a866e6ebb27b62fb75b9047dafcda53be4147a3
vishnumohanrk <mohanvish1999@gmail.com>
2025-01-06 12:16:44 +0530

    feat: add button and icon button atom componentss

---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,42 @@
+export type ButtonVariant = 'primary' | 'secondary';
+
+export type ButtonSize = 'sm' | 'md';
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+}
+
+export function Button({
+  className,
+  size = 'md',
+  type = 'button',
+  variant = 'primary',
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      type={type}
+      className={`${getButtonClasses(variant, size)} ${className}`}
+    />
+  );
+}
+
+const buttonBaseClasses =
+  'flex items-center gap-2 px-4 py-2 text-sm font-medium transition-colors disabled:cursor-not-allowed';
+
+const buttonVariantClasses = {
+  primary: 'text-white bg-blue-600 hover:bg-blue-700 disabled:bg-blue-400',
+  secondary: 'text-gray-700 bg-gray-100 hover:bg-gray-200',
+} satisfies Record<ButtonVariant, string>;
+
+const buttonSizeClasses = {
+  sm: 'text-sm rounded-md',
+  md: 'text-base rounded-lg',
+} satisfies Record<ButtonSize, string>;
+
+function getButtonClasses(variant: ButtonVariant, size: ButtonSize) {
+  return `${buttonBaseClasses} ${buttonVariantClasses[variant]} ${buttonSizeClasses[size]}`;
+}
---
+++ src/components/atoms/icon-button.tsx
@@ -0,0 +1,47 @@
+export type IconButtonVariant = 'primary' | 'secondary' | 'success' | 'danger';
+
+export type IconButtonSize = 'sm' | 'md';
+
+export interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: IconButtonSize;
+  variant?: IconButtonVariant;
+}
+
+export function IconButton({
+  className,
+  size = 'md',
+  type = 'button',
+  variant = 'primary',
+  ...props
+}: IconButtonProps) {
+  return (
+    <button
+      {...props}
+      type={type}
+      className={`${getIconButtonClassName(variant, size)} ${className}`}
+    />
+  );
+}
+
+const iconButtonBaseClasses =
+  'rounded-full transition-colors disabled:cursor-not-allowed';
+
+const iconButtonVariantClasses = {
+  primary: 'hover:bg-blue-50 text-blue-500',
+  secondary: 'text-gray-700 bg-gray-100 hover:bg-gray-200',
+  success: 'bg-green-100 text-green-600 hover:bg-green-200',
+  danger: 'bg-red-100 text-red-600 hover:bg-red-200',
+} satisfies Record<IconButtonVariant, string>;
+
+const iconButtonSizeClasses = {
+  sm: 'p-1',
+  md: 'p-2',
+} satisfies Record<IconButtonSize, string>;
+
+function getIconButtonClassName(
+  variant: IconButtonVariant,
+  size: IconButtonSize
+) {
+  return `${iconButtonBaseClasses} ${iconButtonVariantClasses[variant]} ${iconButtonSizeClasses[size]}`;
+}
